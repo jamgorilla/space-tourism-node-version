738d9dc900b65f04b05fb20a860f0fa062d582b9
jamgorilla <jamesmurphy507@hotmail.com>
2022-04-11 15:07:57 +0100

    updated navigation links

--- js/navTemplateLiteral.js
+++ js/navTemplateLiteral.js
@@ -9,9 +9,9 @@ document.write(`<div class="nav-container">
 </div>
 
 <ul class="nav-menu">
-  <li><span class="nav-span">00</span><a href="http://localhost:3000/views/index"> Home</a></li>
-  <li><span class="nav-span">01</span><a href="http://localhost:3000/views/destination"> Destination</a></li>
-  <li><span class="nav-span">02</span><a href="http://localhost:3000/views/crew"> Crew</a></li>
-  <li><span class="nav-span">03</span><a href="http://localhost:3000/views/technology"> Technology</a></li>
+  <li><span class="nav-span">00</span><a href="https://space-tourism-node.herokuapp.com/views/index"> Home</a></li>
+  <li><span class="nav-span">01</span><a href="https://space-tourism-node.herokuapp.com/views/destination"> Destination</a></li>
+  <li><span class="nav-span">02</span><a href="https://space-tourism-node.herokuapp.com/views/crew"> Crew</a></li>
+  <li><span class="nav-span">03</span><a href="https://space-tourism-node.herokuapp.com/views/technology"> Technology</a></li>
 </ul> 
 `);
