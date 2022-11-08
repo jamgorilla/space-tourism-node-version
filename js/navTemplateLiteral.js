3140475e1fd3b531c7c13d06646ca7b0d24195b7
jamgorilla <jamesmurphy507@hotmail.com>
2022-11-08 16:14:17 +0000

    switched hardcoded heroku links to vercel

--- js/navTemplateLiteral.js
+++ js/navTemplateLiteral.js
@@ -9,9 +9,9 @@ document.write(`<div class="nav-container">
 </div>
 
 <ul class="nav-menu">
-  <li><span class="nav-span">00</span><a href="https://space-tourism-node.herokuapp.com/views/index"> Home</a></li>
-  <li><span class="nav-span">01</span><a href="https://space-tourism-node.herokuapp.com/views/destination"> Destination</a></li>
-  <li><span class="nav-span">02</span><a href="https://space-tourism-node.herokuapp.com/views/crew"> Crew</a></li>
-  <li><span class="nav-span">03</span><a href="https://space-tourism-node.herokuapp.com/views/technology"> Technology</a></li>
+  <li><span class="nav-span">00</span><a href="https://space-tourism-node-version.vercel.app/views/index"> Home</a></li>
+  <li><span class="nav-span">01</span><a href="https://space-tourism-node-version.vercel.app/views/destination"> Destination</a></li>
+  <li><span class="nav-span">02</span><a href="https://space-tourism-node-version.vercel.app/views/crew"> Crew</a></li>
+  <li><span class="nav-span">03</span><a href="https://space-tourism-node-version.vercel.app/views/technology"> Technology</a></li>
 </ul> 
 `);
