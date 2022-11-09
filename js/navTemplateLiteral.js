955c0a5765569efde68353b99808aa154c65dcbc
jamgorilla <jamesmurphy507@hotmail.com>
2022-11-09 15:47:07 +0000

    added html to links

--- js/navTemplateLiteral.js
+++ js/navTemplateLiteral.js
@@ -9,9 +9,9 @@ document.write(`<div class="nav-container">
 </div>
 
 <ul class="nav-menu">
-  <li><span class="nav-span">00</span><a href="https://space-tourism-node-version.vercel.app/views/index"> Home</a></li>
-  <li><span class="nav-span">01</span><a href="https://space-tourism-node-version.vercel.app/views/destination"> Destination</a></li>
-  <li><span class="nav-span">02</span><a href="views/crew"> Crew</a></li>
-  <li><span class="nav-span">03</span><a href="https://space-tourism-node-version.vercel.app/views/technology"> Technology</a></li>
+  <li><span class="nav-span">00</span><a href="views/index.html"> Home</a></li>
+  <li><span class="nav-span">01</span><a href="views/destination.html"> Destination</a></li>
+  <li><span class="nav-span">02</span><a href="views/crew.html"> Crew</a></li>
+  <li><span class="nav-span">03</span><a href="views/technology.html"> Technology</a></li>
 </ul> 
 `);
