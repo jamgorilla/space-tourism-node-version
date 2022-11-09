e2ebc4390420a97ac71fe056cb2e38a7bbcae37e
jamgorilla <jamesmurphy507@hotmail.com>
2022-11-09 14:44:26 +0000

    added relative path to crew link

--- js/navTemplateLiteral.js
+++ js/navTemplateLiteral.js
@@ -11,7 +11,7 @@ document.write(`<div class="nav-container">
 <ul class="nav-menu">
   <li><span class="nav-span">00</span><a href="https://space-tourism-node-version.vercel.app/views/index"> Home</a></li>
   <li><span class="nav-span">01</span><a href="https://space-tourism-node-version.vercel.app/views/destination"> Destination</a></li>
-  <li><span class="nav-span">02</span><a href="https://space-tourism-node-version.vercel.app/views/crew"> Crew</a></li>
+  <li><span class="nav-span">02</span><a href="views/crew"> Crew</a></li>
   <li><span class="nav-span">03</span><a href="https://space-tourism-node-version.vercel.app/views/technology"> Technology</a></li>
 </ul> 
 `);
